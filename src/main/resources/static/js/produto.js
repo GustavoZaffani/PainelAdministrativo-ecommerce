0de7e29048b2cd71fc858c124fc7e1e0a251728d
Gustavo Zaffani <g_zaffani@hotmail.com>
2019-11-27 21:43:32 -0300

    commit heroku

--- src/main/resources/static/js/produto.js
+++ src/main/resources/static/js/produto.js
@@ -2,32 +2,36 @@ $(function () {
     validaProduto('/produto', '#formProduto');
 });
 
-function saveProduto(e) {
-    e.preventDefault();
+$('#formProduto').submit(function (e) {
 
-    var formData = new FormData($('#formProduto')[0]);
+        e.preventDefault();
+        var formData = new FormData($('#formProduto')[0]);
 
-    $.ajax({
-        type: $('#formProduto').attr('method'),
-        url: $('#formProduto').attr('action'),
-        data: formData,
-        success: function () {
-            swal({
-                title: 'Salvo!',
-                text: 'Registro salvo com sucesso!',
-                type: 'success'
-            }, function () {
-                window.location = '/';
-            });
-        },
-        error: function (data) {
-            console.log(data);
-            swal(
-                'Atenção!',
-                'Ocorreu um erro ao salvar o registro. Por favor, tente novamente!',
-                'error'
-            );
-        }
-    });
-    return false;
-}
+        $.ajax({
+            type: $('#formProduto').attr('method'),
+            url: $('#formProduto').attr('action'),
+            data: formData,
+            async: false,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function () {
+                swal({
+                    title: 'Salvo!',
+                    text: 'Registro salvo com sucesso!',
+                    type: 'success'
+                }, function () {
+                    window.location = '/';
+                });
+            },
+            error: function (data) {
+                console.log(data);
+                swal(
+                    'Atenção!',
+                    'Ocorreu um erro ao salvar o registro. Por favor, tente novamente!',
+                    'error'
+                );
+            }
+        });
+        return false;
+});
